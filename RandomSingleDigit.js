c7a0c9c787300e386c1fe9cdeab2469a7afc779a
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-09 01:57:22 +0530

    Added: Checking Leap year

--- RandomSingleDigit.js
+++ RandomSingleDigit.js
@@ -1,29 +1,21 @@
-var date1 = "03/20";
-var date2 = "06/20";
-
 const prompt = require('prompt-sync')();
 
-var date = prompt('Enter the date in MM/DD/YYYY format: ');
-let str1 = date1.split("/");
-let str2 = date2.split("/");
-let str3 = date.split("/");
-
-if(str1[0] <= str3[0] && str2[0] >= str3[0]){
-    if(str3[0] == str1[0]){
-        if(str3[1] > str1[1])
-            console.log("True");
+let year = prompt("Enter the year: ");
+let check_leap = false;
+if(year % 4 == 0){
+    if( year % 100 == 0){
+        if ( year % 400 == 0)
+            check_leap = true;
         else
-            console.log("False");
-    }
-    else if(str3[0] == str2[0]){
-        if(str3[1] < str2[1])
-            console.log("True");
-        else
-            console.log("False");
-    }
-
+            check_leap = false;
+        }
     else
-        console.log("True");
-}
+        check_leap = true;
+    }
 else
-    console.log("False");
+    check_leap = false;
+
+if(check_leap)
+    console.log("The given year is a leap year.")
+else
+console.log("The given year is not a leap year.")
