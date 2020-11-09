8732dd2ff68117a9320a3d99a268a87506cdedbd
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 02:06:00 +0530

    Addes: Square of a number using while loop

--- Programs.js
+++ Programs.js
@@ -1,6 +1,10 @@
 const prompt = require('prompt-sync')();
 
 let input = prompt("Enter a number : ");
-for(let i=0; i<= input; i++){
+let count = 0;
+let i = 1;
+while(Math.pow(2,i) <= 256 && count <= input){
     console.log(Math.pow(2,i));
+    i ++;
+    count ++;
 }
