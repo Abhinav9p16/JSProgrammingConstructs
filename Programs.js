cd15c9333dd45211cc56fc5f231d4aade993fbaf
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-09 08:58:59 +0530

    Added: Finding Minimum Maximum

--- Programs.js
+++ Programs.js
@@ -1,19 +1,38 @@
 const prompt = require('prompt-sync')();
 
-let num = prompt("Enter a number between 1-7: ");
-if(num == 1)
-    console.log("Sunday")
-else if(num == 2)
-    console.log("Monday")
-else if(num == 3)
-    console.log("Tuesday")
-else if(num == 4)
-    console.log("Wednesday")
-else if(num == 5)
-    console.log("Thursday")
-else if(num == 6)
-    console.log("Friday")
-else if(num == 7)
-    console.log("Saturday")
-else
-    console.log("Wrong Input!")
+let num1 = prompt("Enter the first number: ");
+let num2 = prompt("Enter the second number: ");
+let num3 = prompt("Enter the third number: ");
+
+let op1_result = Integer.parseInt(num1) + num2 * num3;
+let op2_result = num1 % num2 + Integer.parseInt(num3);
+let op3_result = Integer.parseInt(num3) + num1 / num2;
+let op4_result = num1 * num2 + Integer.parseInt(num3);
+
+let min = op1_result;
+let max = op1_result;
+
+if(op1_result > op2_result && op1_result > op3_result && op1_result > op4_result)
+    max = op1_result;
+else if(op2_result > op3_result && op2_result > op4_result)
+    max = op2_result;
+else if(op3_result > op4_result)
+    max = op3_result;
+else   
+    max = op4_result;
+
+if(op1_result < op2_result && op1_result < op3_result && op1_result < op4_result)
+    min = op1_result;
+else if(op2_result < op3_result && op2_result < op4_result)
+    min = op2_result;
+else if(op3_result < op4_result)
+    min = op3_result;
+else   
+    min = op4_result;
+
+console.log("op1_result " + op1_result)
+console.log("op2_result " + op2_result)
+console.log("op3_result " + op3_result)
+console.log("op4_result " + op4_result)
+console.log("Minimum: " + min)
+console.log("Maximum: " + max)
