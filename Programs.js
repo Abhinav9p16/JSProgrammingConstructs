921ef4a2942583b087f21fd51291e8fdd1f8592a
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 02:08:45 +0530

    Added: Prime numbers between 2 numbers

--- Programs.js
+++ Programs.js
@@ -1,10 +1,12 @@
 const prompt = require('prompt-sync')();
 
-let input = prompt("Enter a number : ");
-let count = 0;
-let i = 1;
-while(Math.pow(2,i) <= 256 && count <= input){
-    console.log(Math.pow(2,i));
-    i ++;
-    count ++;
+let input1 = prompt("Enter first number : ");
+let input2 = prompt("Enter last number : ");
+for(let j = input1; j <= input2; j++){
+    for(let i = 2 ; i <= Math.sqrt(j); i++){
+        if(j % i == 0){
+            console.log("Num: " + j + "   Not A Prime");
+        }
+    }
 }
+console.log("Is A Prime Number");
