f5a2cc13caa533017e9ca800d546fbf97fb368df
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 02:14:45 +0530

    Added:Case for unit conversion

--- Programs.js
+++ Programs.js
@@ -1,10 +1,25 @@
 const prompt = require('prompt-sync')();
-let num = prompt("Enter A Number: ");
 
-console.log("Prime Factors of "+ num + " Are: ");
-for(let i = 2; i <= num; i++){
-    while(num % i == 0){
-        console.log("Factor: " + i);
-        num = num / i;
-    }
+console.log("1. Feet to Inch \n2. Inch To Feet \n3. Feet To Meter \n4. Meter To Feet");
+let choice = prompt("Enter a choice: ");
+choice = parseInt(choice);
+switch(choice){
+    case 1:
+        let num1 = prompt("Enter the number in Feet : ");
+        console.log(num1 * 12);
+        break;
+    case 2:
+        let num2 = prompt("Enter the number in Inch : ");
+        console.log(num2 / 12);
+        break;
+    case 3:
+        let num3 = prompt("Enter the number in Feet : ");
+        console.log(num3 * 3 / 10);
+        break;
+    case 4:
+        let num4 = prompt("Enter the number in Meter : ");
+        console.log(num4 * 10 / 3);
+        break;
+    default:
+        console.log("Wrong Input !");
 }
