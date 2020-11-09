ad3e5b6b9bd523eda9ea7c3680197fe2ccc8a05b
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-09 09:05:36 +0530

    Added: Switch Case fot Num to word

--- Programs.js
+++ Programs.js
@@ -1,38 +1,47 @@
 const prompt = require('prompt-sync')();
 
-let num1 = prompt("Enter the first number: ");
-let num2 = prompt("Enter the second number: ");
-let num3 = prompt("Enter the third number: ");
-
-let op1_result = Integer.parseInt(num1) + num2 * num3;
-let op2_result = num1 % num2 + Integer.parseInt(num3);
-let op3_result = Integer.parseInt(num3) + num1 / num2;
-let op4_result = num1 * num2 + Integer.parseInt(num3);
-
-let min = op1_result;
-let max = op1_result;
-
-if(op1_result > op2_result && op1_result > op3_result && op1_result > op4_result)
-    max = op1_result;
-else if(op2_result > op3_result && op2_result > op4_result)
-    max = op2_result;
-else if(op3_result > op4_result)
-    max = op3_result;
-else   
-    max = op4_result;
-
-if(op1_result < op2_result && op1_result < op3_result && op1_result < op4_result)
-    min = op1_result;
-else if(op2_result < op3_result && op2_result < op4_result)
-    min = op2_result;
-else if(op3_result < op4_result)
-    min = op3_result;
-else   
-    min = op4_result;
-
-console.log("op1_result " + op1_result)
-console.log("op2_result " + op2_result)
-console.log("op3_result " + op3_result)
-console.log("op4_result " + op4_result)
-console.log("Minimum: " + min)
-console.log("Maximum: " + max)
+let num1 = prompt("Enter the number between 0-9: ");
+switch(num1){
+    case 0:{
+        console.log("Zero");
+        break;
+    }
+    case 1:{
+        console.log("One");
+        break;
+    }
+    case 2:{
+        console.log("Two");
+        break;
+    }
+    case 3:{
+        console.log("Three");
+        break;
+    }
+    case 4:{
+        console.log("Four");
+        break;
+    }
+    case 5:{
+        console.log("Five");
+        break;
+    }
+    case 6:{
+        console.log("Six");
+        break;
+    }
+    case 7:{
+        console.log("Seven");
+        break;
+    }
+    case 8:{
+        console.log("Eight");
+        break;
+    }
+    case 9:{
+        console.log("Nine");
+        break;
+    }
+    default:
+        console.log("Wrong Input !")
+}
