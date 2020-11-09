8b78c4f0c937307d895ee54719ac104337341c15
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 01:53:31 +0530

    Added: Code to find Magic number

--- Programs.js
+++ Programs.js
@@ -1,9 +1,17 @@
 const prompt = require('prompt-sync')();
 
-let input = prompt("Enter a number : ");
-let sum = 0;
-for(let i = 1; i <= input; i++){
-    sum = sum + (1/i);
+let input = prompt("Enter a number between 1 and 100 : ");
+let upper = 100;
+let lower = 1;
+let mid = 0;
+while(mid != input){
+    mid  = (upper + lower) / 2;
+    if(input < mid){
+        upper = mid;
+    }
+    else{
+        lower = mid;
+    }
 }
 
-console.log(sum);
+console.log(mid);
