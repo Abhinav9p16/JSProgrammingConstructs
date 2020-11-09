42041833c1a0054cc7541b8e4ef39bf7b4a4202e
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 01:51:07 +0530

    Added: Harmonic series

--- Programs.js
+++ Programs.js
@@ -1,19 +1,9 @@
-let BET = 1;
-let WIN = 1;
-let stash = 100;
+const prompt = require('prompt-sync')();
 
-while (stash != 0 && stash != 200){
-    let choice = Math.floor(Math.random() * 2) % 2;
-    if(choice == BET){
-        let result = Math.floor(Math.random() * 2) % 2;
-        if(result == WIN)
-            stash++ ;
-        else
-            stash-- ;
-    }
+let input = prompt("Enter a number : ");
+let sum = 0;
+for(let i = 1; i <= input; i++){
+    sum = sum + (1/i);
 }
 
-if(stash == 0)
-    console.log("The Gambler Lost !!")
-else if(stash == 200)
-    console.log("The Gambler Won !!")
+console.log(sum);
